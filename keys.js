a84a2bb1e20ff97f42cedf5ee1b7ed3a88116936
Rahul Nallappa <you@example.com>
2017-04-12 13:34:26 -0500

    tweet and song commands working

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<R3K8cz16rnQ1Oh4ZqeLM7bUDu>',
-  consumer_secret: '<j4uVzcOFP7BeLtAXeNT183liihSCCeugBuhdJY081jJePm4awF>',
-  access_token_key: '<430387314-CCJP9gx8JbBa9IihTjaUY92cr4rbj2cX6NvudA3g>',
-  access_token_secret: '<L529sp7pRhqQABPMUe7YxbiJZSSqGBvAcVbyZ4sO3qnNV>',
+  consumer_key: 'R3K8cz16rnQ1Oh4ZqeLM7bUDu',
+  consumer_secret: 'j4uVzcOFP7BeLtAXeNT183liihSCCeugBuhdJY081jJePm4awF',
+  access_token_key: '430387314-CCJP9gx8JbBa9IihTjaUY92cr4rbj2cX6NvudA3g',
+  access_token_secret: 'L529sp7pRhqQABPMUe7YxbiJZSSqGBvAcVbyZ4sO3qnNV'
 }
--- liri.js
+++ liri.js
@@ -18,9 +18,10 @@ function displayTweets() {
   	console.log('An error has occurred: ' + error);
   	return;
   }	else {
-    console.log('Here are the most recent tweets from ' + tweets[0].user.name);
+    console.log('Here are the most recent tweets from ' + tweets[0].user.name + ":");
     	for (var i = 0; i < 20; i++) {
     		console.log('----------------------');
+    		console.log("#" + (i+1));
     		console.log('At ' + tweets[i].created_at);
     		console.log(tweets[i].text);
     	};
@@ -32,22 +33,41 @@ function displayTweets() {
 function spotifyThisSong (song) {
 	spotify.search({ type: 'track', query: song}, function(err, data) {
     if (err) {
-        console.log('Error occurred: ' + err);
+        console.log('An error has occurred: ' + err);
         return;
-    }
+     } else {
+     	var songInfo = data.tracks.items[0];
+     	var songResult = ["Artist: " + songInfo.artists[0].name, "Song: " + songInfo.name, "URL: " + songInfo.preview_url, "Album Name: " + songInfo.album.name];
+     	for(var i = 0; i < 4; i++) {
+     		console.log('----------------------');
+     		console.log(songResult[i]);
+     	}
+     }
+  });
 }
 
 
 function movieThis (movie) {
+
 	
 }
+
+function doWhatItSays ()
 
 if (process.argv[2] === "my-tweets") {
 	displayTweets();
 	};
 
 if (process.argv[2] === "spotify-this-song") {
+	if (process.argv[3] == null) {
+		var songResult = ["Artist: " + "Ace of Base", "URL: " + "https://play.spotify.com/artist/5ksRONqssB7BR161NTtJAm?play=true&utm_source=open.spotify.com&utm_medium=open", "Album Name: " + "The Sign"];
+		for(var i = 0; i < 3; i++) {
+     		console.log('----------------------');
+     		console.log(songResult[i]);
+     	}
+	} else {
 	spotifyThisSong(process.argv[3]);
+	}
 };
 
 if(process.argv[2] === "movie-this") {
