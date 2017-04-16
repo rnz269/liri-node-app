5f2503dd5733406709b79fa5720a0ff46f3b6b4a
Rahul Nallappa <you@example.com>
2017-04-16 14:29:36 -0500

    final commit

--- liri.js
+++ liri.js
@@ -24,13 +24,13 @@ function displayTweets() {
     		console.log("#" + (i+1));
     		console.log('At ' + tweets[i].created_at);
     		console.log(tweets[i].text);
-    	};
+    	}
 
-  };
-});
+  }
+})
 }
 
-function spotifyThisSong (song) {
+function spotifyThisSong(song) {
 	spotify.search({ type: 'track', query: song}, function(err, data) {
     if (err) {
         console.log('An error has occurred: ' + err);
@@ -43,37 +43,86 @@ function spotifyThisSong (song) {
      		console.log(songResult[i]);
      	}
      }
-  });
+  })
 }
 
 
-function movieThis (movie) {
-
-	
+function movieThis(movie) {
+	var searchQuery = "http://www.omdbapi.com/?" + "t=" + movie;
+	request(searchQuery, function (error, response, body) {
+		// console.log('error:', error);
+		// console.log('statusCode:', response && response.statusCode);
+		if (!error && response.statusCode === 200) {
+			console.log("Title: " + JSON.parse(body).Title);
+			console.log('----------------------');
+			console.log("Year Produced: " + JSON.parse(body).Year)
+			console.log('----------------------');
+			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			console.log('----------------------');
+			console.log("Country of Production: " + JSON.parse(body).Country);
+			console.log('----------------------');
+			console.log("Language: " + JSON.parse(body).Language);
+			console.log('----------------------');
+			console.log("Plot: " + "\n" + JSON.parse(body).Plot);
+			console.log('----------------------');
+			console.log("Actors: " + JSON.parse(body).Actors);
+			console.log('----------------------');
+			console.log("Title: " + JSON.parse(body).Title);
+			console.log('----------------------');
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Source);
+			console.log('----------------------');
+			console.log("Rotten Tomatoes URL: " + JSON.parse(body).Website);
+			console.log('----------------------');
+			} else {
+			console.log("No movie returned.");
+		}
+	});	
 }
 
-function doWhatItSays ()
+function doWhatItSays() {
+	fs.readFile('random.txt', function (err, data) {
+		if (err) {
+			return console.error(err);
+		} else {
+			var textString = data.toString();
+			var dataArray = textString.split(",");
+			if (dataArray[0] === "spotify-this-song") {
+				spotifyThisSong(dataArray[1]);
+			}
+		}
+	})
+}
 
-if (process.argv[2] === "my-tweets") {
+var desiredProgram = process.argv[2];
+var input = process.argv[3];
+
+if (desiredProgram === "my-tweets") {
 	displayTweets();
-	};
-
-if (process.argv[2] === "spotify-this-song") {
-	if (process.argv[3] == null) {
-		var songResult = ["Artist: " + "Ace of Base", "URL: " + "https://play.spotify.com/artist/5ksRONqssB7BR161NTtJAm?play=true&utm_source=open.spotify.com&utm_medium=open", "Album Name: " + "The Sign"];
-		for(var i = 0; i < 3; i++) {
-     		console.log('----------------------');
-     		console.log(songResult[i]);
-     	}
-	} else {
-	spotifyThisSong(process.argv[3]);
 	}
-};
 
-if(process.argv[2] === "movie-this") {
-
-
-
+if (desiredProgram === "spotify-this-song") {
+	if (input == null) {
+		spotify.lookup({ type: 'track', id: "0hrBpAOgrt8RXigk83LLNE"}, function(err, data){
+			// console.log(data.artists[0].name + "\n" + data.name + "\n" + data.preview_url + "\n" + data.album.name);
+     	var songResult = ["Artist: " + data.artists[0].name, "Song: " + data.name, "URL: " + data.preview_url, "Album Name: " + data.album.name];
+	     	for(var i = 0; i < 4; i++) {
+	     		console.log('----------------------');
+	     		console.log(songResult[i]);
+			}
+	})
+	} else {
+	spotifyThisSong(input);
+	}
 }
 
+if(desiredProgram === "movie-this") {
+	if (input == null) {
+		movieThis("Mr. Nobody");
+	} else {
+		movieThis(input);
+	}
+}
 
+if(desiredProgram === "do-what-it-says") {
+	doWhatItSays();
+}
